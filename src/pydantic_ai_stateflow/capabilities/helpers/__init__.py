a3b59756f73dc986e7f04137b8828600526fda8c
Kir <5420754+lastonoga@users.noreply.github.com>
2026-05-18 23:39:01 +0400

    feat(capabilities): TypedLoopGuard (Pattern-side typed field loop check)

--- src/pydantic_ai_stateflow/capabilities/helpers/__init__.py
+++ src/pydantic_ai_stateflow/capabilities/helpers/__init__.py
@@ -3,5 +3,11 @@ from pydantic_ai_stateflow.capabilities.helpers.semantic_deduper import (
     SemanticDeduper,
     SemanticLoopDetected,
 )
+from pydantic_ai_stateflow.capabilities.helpers.typed_loop_guard import TypedLoopGuard
 
-__all__ = ["Embedder", "SemanticDeduper", "SemanticLoopDetected"]
+__all__ = [
+    "Embedder",
+    "SemanticDeduper",
+    "SemanticLoopDetected",
+    "TypedLoopGuard",
+]
